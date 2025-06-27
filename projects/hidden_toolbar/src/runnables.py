594d33a8eab3389c5d9db3b9c6ddb80b1d1b13e5
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 20:38:26 +0200

    Add loading label to ProgramLauncher and update with first program name

--- projects/hidden_toolbar/src/runnables.py
+++ projects/hidden_toolbar/src/runnables.py
@@ -10,6 +10,14 @@ class ProgramLauncher(Gtk.Window):
         super().__init__(title="Program Launcher")
         self.set_default_size(800, 600)
         self.connect("destroy", Gtk.main_quit)
+        # Create a label with placeholder text
+        self.label = Gtk.Label(label="Loading...")
+        self.add(self.label)
+        self.show_all()
+        # After 1 second, update the label with the first program name
+        Gtk.timeout_add(1000, self.update_label_with_first_program)
+
+    def update_label_with_first_program(self):
         scanned_path = os.path.join(os.path.dirname(__file__), "scanned_programs.json")
         first_program = "No program found"
         if os.path.exists(scanned_path):
@@ -20,9 +28,8 @@ class ProgramLauncher(Gtk.Window):
                         first_program = next(iter(data.keys()))
                 except Exception:
                     pass
-        label = Gtk.Label(label=first_program)
-        self.add(label)
-        self.show_all()
+        self.label.set_text(first_program)
+        return False  # Only run once
 
 def show_launcher(programs=None):
     win = ProgramLauncher(programs)
