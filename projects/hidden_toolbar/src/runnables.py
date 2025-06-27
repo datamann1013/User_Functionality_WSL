2930b71a10db5b02ca12d60a59ecddc4b4d1031d
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 20:14:52 +0200

    Add program listing from scanned_programs.json in runnables.py

--- projects/hidden_toolbar/src/runnables.py
+++ projects/hidden_toolbar/src/runnables.py
@@ -1,4 +1,6 @@
 import gi
+import os
+import json
 
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -7,7 +9,21 @@ class ProgramLauncher(Gtk.Window):
     def __init__(self, programs=None):
         super().__init__(title="Program Launcher")
         self.set_default_size(800, 600)
-        self.connect("destroy", Gtk.main_quit)
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.add(vbox)
+        # Load scanned programs
+        scanned_path = os.path.join(os.path.dirname(__file__), "scanned_programs.json")
+        programs_list = []
+        if os.path.exists(scanned_path):
+            with open(scanned_path, "r") as f:
+                data = json.load(f)
+                programs_list = list(data.keys())[:10]
+        else:
+            programs_list = ["No scanned_programs.json found"]
+        # Display first 10 programs
+        for prog in programs_list:
+            label = Gtk.Label(label=prog)
+            vbox.pack_start(label, False, False, 0)
         self.show_all()
 
 def show_launcher(programs=None):
