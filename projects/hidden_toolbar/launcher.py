723ee0050352da6e428c426e106de7032ddd8323
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 19:29:49 +0200

    Refactor launcher.py to replace runnable retrieval with HiddenToolbar initialization

--- projects/hidden_toolbar/launcher.py
+++ projects/hidden_toolbar/launcher.py
@@ -1,33 +1,12 @@
 import os
 import sys
-from .src.runnables import show_launcher
-
-def get_linux_runnables():
-    paths = os.environ.get("PATH", "").split(os.pathsep)
-    seen = set()
-    programs = []
-    for path in paths:
-        if not os.path.isdir(path):
-            continue
-        for fname in os.listdir(path):
-            fpath = os.path.join(path, fname)
-            if fname in seen:
-                continue
-            if os.path.isfile(fpath) and os.access(fpath, os.X_OK):
-                # Filter out python files in this project
-                if fname.endswith(".py") and os.path.dirname(fpath) == os.path.abspath(os.path.dirname(__file__)):
-                    continue
-                # Exclude Windows .exe apps
-                if fname.lower().endswith('.exe'):
-                    continue
-                seen.add(fname)
-                programs.append(fname)
-    programs = sorted(set(programs), key=lambda x: x.lower())
-    return programs
+from .src.panel import HiddenToolbar
+from gi.repository import Gtk
 
 def main():
-    programs = get_linux_runnables()
-    show_launcher(programs)
+    win = HiddenToolbar()
+    win.connect("destroy", Gtk.main_quit)
+    Gtk.main()
 
 if __name__ == "__main__":
     main()
