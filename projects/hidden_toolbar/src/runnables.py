e6aeb80ee93a6247f8e4e5fc45fb53f76d76189d
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 22:17:28 +0200

    Refactor button implementation in Runnables to use EventBox for improved event handling

--- projects/hidden_toolbar/src/runnables.py
+++ projects/hidden_toolbar/src/runnables.py
@@ -64,27 +64,25 @@ class HiddenToolbar(Gtk.Window):
         inner_box.set_halign(Gtk.Align.CENTER)
         inner_box.set_valign(Gtk.Align.CENTER)
 
-        terminal_label = Gtk.Label()
-        terminal_label.set_markup("<b>Terminal</b>")
-        terminal_button = Gtk.Button()
-        terminal_button.add(terminal_label)
-        terminal_button.connect("clicked", lambda w: run_command("xterm"))
+        # Use EventBox with a label for each button
+        terminal_event = Gtk.EventBox()
+        terminal_label = Gtk.Label(label="Terminal")
+        terminal_event.add(terminal_label)
+        terminal_event.connect("button-press-event", lambda w, e: run_command("xterm"))
 
-        filemanager_label = Gtk.Label()
-        filemanager_label.set_markup("<i>File Manager</i>")
-        filemanager_button = Gtk.Button()
-        filemanager_button.add(filemanager_label)
-        filemanager_button.connect("clicked", lambda w: run_command("explorer.exe ~"))
+        filemanager_event = Gtk.EventBox()
+        filemanager_label = Gtk.Label(label="File Manager")
+        filemanager_event.add(filemanager_label)
+        filemanager_event.connect("button-press-event", lambda w, e: run_command("explorer.exe ~"))
 
-        launcher_label = Gtk.Label()
-        launcher_label.set_markup("<span foreground='yellow'>Launcher</span>")
-        launcher_button = Gtk.Button()
-        launcher_button.add(launcher_label)
-        launcher_button.connect("clicked", self.launch_runnables)
+        launcher_event = Gtk.EventBox()
+        launcher_label = Gtk.Label(label="Launcher")
+        launcher_event.add(launcher_label)
+        launcher_event.connect("button-press-event", self.launch_runnables)
 
-        inner_box.pack_start(terminal_button, False, False, 0)
-        inner_box.pack_start(filemanager_button, False, False, 0)
-        inner_box.pack_start(launcher_button, False, False, 0)
+        inner_box.pack_start(terminal_event, False, False, 0)
+        inner_box.pack_start(filemanager_event, False, False, 0)
+        inner_box.pack_start(launcher_event, False, False, 0)
 
         outer_box.pack_start(inner_box, True, True, 0)
 
