e18481040c0f1555260ce380cc2f73981c65aa7b
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 19:27:32 +0200

    Refactor ProgramLauncher to allow optional programs parameter and connect destroy signal in constructor

--- projects/hidden_toolbar/src/runnables.py
+++ projects/hidden_toolbar/src/runnables.py
@@ -4,12 +4,12 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class ProgramLauncher(Gtk.Window):
-    def __init__(self, programs):
+    def __init__(self, programs=None):
         super().__init__(title="Program Launcher")
         self.set_default_size(800, 600)
+        self.connect("destroy", Gtk.main_quit)
         self.show_all()
 
-def show_launcher(programs):
+def show_launcher(programs=None):
     win = ProgramLauncher(programs)
-    win.connect("destroy", Gtk.main_quit)
     Gtk.main()
