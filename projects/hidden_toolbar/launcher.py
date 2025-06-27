6cfa55525bdd660fac45c7d958464cdbc26b2ced
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-27 19:44:54 +0200

    Fix import statement for HiddenToolbar to use relative path

--- projects/hidden_toolbar/launcher.py
+++ projects/hidden_toolbar/launcher.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from src.panel import HiddenToolbar
+from .src.panel import HiddenToolbar
 from gi.repository import Gtk
 
 def main():
@@ -8,5 +8,5 @@ def main():
     win.connect("destroy", Gtk.main_quit)
     Gtk.main()
 
-if __name__ == "__main__" or __name__ == "projects.hidden_toolbar.launcher":
+if __name__ == "__main__":
     main()
