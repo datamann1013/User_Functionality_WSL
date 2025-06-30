72201b53d776a6bc7ff6fc66788e0e6a44d62a3c
datamann1013 <141662792+datamann1013@users.noreply.github.com>
2025-06-30 19:52:10 +0200

    Update import paths for HiddenToolbar and utils to reflect new project structure

--- projects/hidden_toolbar/launcher.py
+++ projects/hidden_toolbar/launcher.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
-from .src.panel import HiddenToolbar
-from .src.utils import scan_development_programs_background, ensure_x_server
+from projects.hidden_toolbar.src.panel import HiddenToolbar
+from projects.hidden_toolbar.src.utils import scan_development_programs_background, ensure_x_server
 from gi.repository import Gtk
 
 def main():
