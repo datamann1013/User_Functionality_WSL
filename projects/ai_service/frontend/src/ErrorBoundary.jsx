e43dd510ac83c7caa1449365dd7503c293990f6d
datamannen1013 <data0@rommesmo.no>
2025-07-20 17:33:12 +0200

    Add ErrorBoundary component for frontend error handling

---
+++ projects/ai_service/frontend/src/ErrorBoundary.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { logFrontendError } from "./utils/errorLogger";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  async componentDidCatch(error, info) {
+    await logFrontendError({ error, info: info.componentStack });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h2>Something went wrong.</h2>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
