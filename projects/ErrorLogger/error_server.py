33fbb82de83c337ad8020881ad34a26bfe16b8c6
datamannen1013 <data0@rommesmo.no>
2025-07-22 15:52:00 +0200

    Update error logging to use remote logger: replace local log_error with log_error_remote in error_server.py

--- projects/ErrorLogger/error_server.py
+++ projects/ErrorLogger/error_server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from logger import log_error
+from logger import log_error_remote
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ def log():
     error_code = data.get('error_code', 0)
     exception = data.get('exception', '')
     extra = data.get('extra', None)
-    log_error(error_code, exception=exception, extra=extra)
+    log_error_remote(error_code, exception=exception, extra=extra)
     return jsonify({'status': 'logged'}), 200
 
 if __name__ == '__main__':
